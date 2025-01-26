af6e6ca6986eff3d8436d82f11f18ec112ee3e17
JL200126 <jefferyliu6@gmail.com>
2025-01-25 19:27:34 -0500

    17

--- Day17/main.py
+++ Day17/main.py
@@ -1,8 +1,11 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
 for i in question_data:
     new_question = Question(i["text"],i["answer"])
     question_bank.append(new_question)
-print(question_bank[0].question)
+
+quiz = QuizBrain(question_bank)
+quiz.next_question()
