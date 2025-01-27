2ffd26ddf069960db8279dbbfd4d8a69000023b5
JL200126 <jefferyliu6@gmail.com>
2025-01-26 21:18:45 -0500

    18

--- Day18/main.py
+++ Day18/main.py
@@ -3,6 +3,7 @@ os.environ['TK_SILENCE_DEPRECATION'] = '1'
 
 
 from turtle import Turtle, Screen
+import random
 
 timmy_the_turtle = Turtle()
 #timmy_the_turtle.shape("turtle")
@@ -10,11 +11,14 @@ timmy_the_turtle = Turtle()
 #timmy_the_turtle.color("red")
 #timmy_the_turtle.forward(100)
 #timmy_the_turtle.right(100)
+timmy_the_turtle.width(width=4)
+timmy_the_turtle.speed(speed= 10)
 
-for _ in range(4):
-    
-    timmy_the_turtle.forward(100)
-    timmy_the_turtle.left(90)
+for _ in range(10):
+    dist = random.randint(1,100)
+    dir = random.randint(0,360)
+    timmy_the_turtle.forward(dist)
+    timmy_the_turtle.left(dir)
 
 
 screen = Screen()
