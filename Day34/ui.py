389adbbcb3e15123c4a592fe1f78abe77d92832f
JL200126 <jefferyliu6@gmail.com>
2025-02-11 23:57:28 -0500

    34

--- Day34/ui.py
+++ Day34/ui.py
@@ -1,10 +1,13 @@
-from tkinter import *
-from quiz_brain import QuizBrain
 
-WHITE = "#FFFFFF"
 THEME_COLOR = "#375362"
 TICK_PATH = "Day34/images/true.png"
 CROSS_PATH = "Day34/images/false.png"
+
+
+from tkinter import *
+from quiz_brain import QuizBrain
+
+
 
 
 class QuizInterface:
@@ -14,26 +17,61 @@ class QuizInterface:
 
         self.window = Tk()
         self.window.title("Quizzler")
-        self.window.config(padx=20, pady=20, background=THEME_COLOR)
-        self.score = 0 
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
 
-        self.tickimage = PhotoImage(file = TICK_PATH)
-        self.tick_button = Button(image=self.tickimage, highlightthickness=0)
-        self.tick_button.grid(row=2, column=0)
-        self.crossimage = PhotoImage(file = CROSS_PATH)
-        self.cross_button = Button(image=self.crossimage, highlightthickness=0)
-        self.cross_button.grid(row=2, column=1)
-        
-
-        self.score_label = Label(text= f"Score: {self.score}", bg=THEME_COLOR, fg = WHITE)
+        self.score_label = Label(text="Score: 0", fg="white", bg=THEME_COLOR)
         self.score_label.grid(row=0, column=1)
-        
-        self.canvas = Canvas(width=300, height=250, bg=WHITE)
-        self.question_text = self.canvas.create_text(150,125,width=280,text = "Some", fill = THEME_COLOR, font = ("Arial", 20, "italic"))
-        self.canvas.grid(row=1, column=0, columnspan =2, pady=50)
+
+        self.canvas = Canvas(width=300, height=250, bg="white")
+        self.question_text = self.canvas.create_text(
+            150,
+            125,
+            width=280,
+            text="Some Question Text",
+            fill=THEME_COLOR,
+            font=("Arial", 20, "italic")
+        )
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
+
+        true_image = PhotoImage(file=TICK_PATH)
+        self.true_button = Button(image=true_image, highlightthickness=0, command=self.true_pressed)
+        self.true_button.grid(row=2, column=0)
+
+        false_image = PhotoImage(file=CROSS_PATH)
+        self.false_button = Button(image=false_image, highlightthickness=0, command=self.false_pressed)
+        self.false_button.grid(row=2, column=1)
+
         self.get_next_question()
+
         self.window.mainloop()
 
     def get_next_question(self):
-        q_text = self.quiz.next_question()
-        self.canvas.itemconfig(self.question_text, text=q_text)
+        self.canvas.config(bg="white")
+        if self.quiz.still_has_questions():
+            self.score_label.config(text=f"Score: {self.quiz.score}")
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=q_text)
+        else:
+            self.canvas.itemconfig(self.question_text, text="You've reached the end of the quiz.")
+            self.true_button.config(state="disabled")
+            self.false_button.config(state="disabled")
+
+    def true_pressed(self):
+        self.give_feedback(self.quiz.check_answer("True"))
+
+    def false_pressed(self):
+        is_right = self.quiz.check_answer("False")
+        self.give_feedback(is_right)
+
+    def give_feedback(self, is_right):
+        if is_right:
+            self.canvas.config(bg="green")
+        else:
+            self.canvas.config(bg="red")
+        self.window.after(1000, self.get_next_question)
+
+
+
+
+
+
