981f894429891212671cfaf3081cc7328e6f57ba
JL200126 <jefferyliu6@gmail.com>
2025-04-13 20:13:00 -0400

    main

--- Day61/day-61-starting-files-flask-secrets/main.py
+++ Day61/day-61-starting-files-flask-secrets/main.py
@@ -1,5 +1,10 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField
 
+class LoginForm(FlaskForm):
+    email = StringField('Email')
+    password = StringField('Password')
 '''
 Red underlines? Install the required packages first: 
 Open the Terminal in PyCharm (bottom left). 
@@ -15,11 +20,16 @@ This will install the packages from requirements.txt for this project.
 
 
 app = Flask(__name__)
-
+app.secret_key = "any-string-you-want-just-keep-it-secret"
 
 @app.route("/")
 def home():
     return render_template('index.html')
+
+@app.route("/login")
+def login():
+    login_form = LoginForm()
+    return render_template('login.html', form=login_form)
 
 
 if __name__ == '__main__':
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# 100 Days of Code: The Complete Python Pro Bootcamp (Paused, resume on 10/4)
+# 100 Days of Code: The Complete Python Pro Bootcamp
 
 Welcome to my journey of mastering Python programming by completing 100 unique projects in 100 days! This repository documents my progress, challenges, and achievements as I follow the course [100 Days of Code: The Complete Python Pro Bootcamp](https://www.udemy.com/course/100-days-of-code/).
 
